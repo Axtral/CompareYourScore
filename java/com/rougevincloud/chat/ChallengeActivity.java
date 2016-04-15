8c5314d540e436259db105b125a64935a8aa4de8
g13005245 <louis.gerard@etu.univ-amu.fr>
2016-04-15 11:41:08 +0200

    ChallengeActivity displays scores

--- java/com/rougevincloud/chat/ChallengeActivity.java
+++ java/com/rougevincloud/chat/ChallengeActivity.java
@@ -9,27 +9,38 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.rougevincloud.chat.data_managers.Server;
 import com.rougevincloud.chat.lists.ListChallengeAdapter;
+import com.rougevincloud.chat.lists.ScoreItem;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ChallengeActivity extends ListActivity {
-    ArrayList< HashMap<String, String> > scores = new ArrayList<>();
     private String[] cols = new String[] {"nom", "score"};
+    private List<ScoreItem> scores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("id", String.valueOf(getIntent().getExtras().getInt(ListChallengeAdapter.EXTRA_ID)));
+        int idChallenge = getIntent().getExtras().getInt(ListChallengeAdapter.EXTRA_ID);
         setContentView(R.layout.activity_challenge);
-        for (int i = 0; i < 10; ++i) {
-            scores.add(new HashMap<String, String>());
-            scores.get(i).put("nom", "Louis");
-            scores.get(i).put("score", String.valueOf(i*100));
+
+        scores = Server.findScoresByChallenge(idChallenge);
+        if (scores == null)
+            return;
+        List< HashMap<String, String> > scoresViewContent = new ArrayList<>();
+        for (ScoreItem score : scores) {
+            scoresViewContent.add(new HashMap<String, String>());
+            int index = scoresViewContent.size()-1;
+            scoresViewContent.get(index).put("nom", String.valueOf(score.getUser()));
+            scoresViewContent.get(index).put("score", "Score : " + String.valueOf(score.getScore()));
         }
+
         ListAdapter adapter = new SimpleAdapter(this,
-                scores,
+                scoresViewContent,
                 android.R.layout.two_line_list_item,
                 cols,
                 new int[] {android.R.id.text1, android.R.id.text2});
--- java/com/rougevincloud/chat/data_managers/Server.java
+++ java/com/rougevincloud/chat/data_managers/Server.java
@@ -1,7 +1,11 @@
 package com.rougevincloud.chat.data_managers;
 
+import android.util.Log;
+
 import com.rougevincloud.chat.LoginActivity;
 import com.rougevincloud.chat.lists.ChallengeItem;
+import com.rougevincloud.chat.lists.ScoreItem;
+import com.rougevincloud.chat.lists.User;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -18,6 +22,7 @@ public class Server {
 
     private Server() {}
 
+///////////////////////////////////////////////////////////////////////////////////////////////LOGIN
     public static Boolean pseudoExists(String pseudo, LoginActivity activity) {
         JSONObject result;
         try {
@@ -70,30 +75,6 @@ public class Server {
         }
     }
 
-    public static List<ChallengeItem> findAllChallenges() {
-        try {
-            JSONObject result = new JSONGetter(url + "aze.json").execute().get();
-            if (result == null)
-                throw new ExecutionException("Unreachable server", new Error());
-            return parseChallenges(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public static List<ChallengeItem> findFriendsChallenges(String pseudo) {
-        try {
-            JSONObject result = new JSONGetter(url + "aze.json?pseudo=" + pseudo).execute().get();
-            if (result == null)
-                throw new ExecutionException("Unreachable server", new Error());
-            return parseChallenges(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     public static String hashPasswd(String passwd) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest md = MessageDigest.getInstance("SHA-512");
         byte[] hash =  md.digest((passwd + "azeqsdwxc").getBytes("UTF-8"));
@@ -104,6 +85,31 @@ public class Server {
             sb.append(Integer.toString((hashByte & 0xff) + 0x100, 16).substring(1));
         }
         return sb.toString();
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////CHALLENGES
+
+    public static List<ChallengeItem> findAllChallenges() {
+        return findChallenges("", null);
+    }
+
+    public static List<ChallengeItem> findFriendsChallenges(String pseudo) {
+        return findChallenges("username", pseudo);
+    }
+
+    private static List<ChallengeItem> findChallenges(String getField, String val) {
+        try {
+            String uri = "aze.json";
+            if (val != null)
+                uri += "?"+ getField +"="+ val;
+            JSONObject result = new JSONGetter(url + uri).execute().get();
+            if (result == null)
+                throw new ExecutionException("Unreachable server", new Error());
+            return parseChallenges(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private static List<ChallengeItem> parseChallenges(JSONObject result) throws Exception {
@@ -117,6 +123,48 @@ public class Server {
             String title = item.getString("title");
             String desc = item.getString("desc");
             list.add(new ChallengeItem(id, iconUrl, title, desc));
+        }
+
+        return list;
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////SCORES
+
+    public static List<ScoreItem> findScoresByChallenge(int id) {
+        return findScores(id, "challenge");
+    }
+
+    public static List<ScoreItem> findScoresByUser(int id) {
+        return findScores(id, "user");
+    }
+
+    private static List<ScoreItem> findScores(int id, String getField) {
+        try {
+            JSONObject result = new JSONGetter(/*url + */"http://darkicex3.alwaysdata.net/android_app/show/showScores.php?"+ getField +"=" + id).execute().get();
+            if (result == null)
+                throw new ExecutionException("Unreachable server", new Error());
+            return parseScores(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<ScoreItem> parseScores(JSONObject result) throws Exception {
+        List<ScoreItem> list = new ArrayList<>();
+        Log.d("score json", result.toString());
+        JSONArray data = result.getJSONArray("scores");
+        int i = 0;
+        while (i < data.length()) {
+            JSONObject item = data.getJSONObject(i++);
+            int id = item.getInt("id");
+            int challengeId = item.getInt("challenge_id");
+            int userId = item.getInt("user_id");
+            int score = item.getInt("score");
+            list.add(new ScoreItem(id,
+                    new ChallengeItem(challengeId, null, null, null),       //todo findChallengeById
+                    new User(userId, null, null),                           //todo findUserById
+                    score));
         }
 
         return list;
